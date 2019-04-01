cf0513209dfa06f012d21990e30d48e61152e2c4
Antkeo1 <sleav143@gmail.com>
2019-04-01 11:24:46 -0400

    connected middleware thunk to redux store in root index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,12 @@
+import jsonPlaceHolder from '../api/jsonPlaceHolder'
+
 export const fetchPosts = () => {
-  return {
-    type: 'FETCH_POST'
+  return function(dispatch, getState) {
+    const promise = jsonPlaceHolder.get('./posts')
+
+    return {
+      type: 'FETCH_POST',
+      payload: promise
+    }
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 import App from './Components/App.js'
-import reducers from './reducers'
+import reducers from './reducers'\
+
+// connect middleware/thunk to redux store
+const store = createStore(reducers, applyMiddleware(thunk))
 
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.querySelector('#root')
