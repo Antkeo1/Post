e62496300154b1c2c440bd47efd2455db38f439f
Antkeo1 <sleav143@gmail.com>
2019-04-06 11:50:51 -0400

    created fetch user action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,3 +9,12 @@ export const fetchPosts = () =>
       payload: res.data
     })
   }
+
+  export const fetchUser = (id) => async dispatch => {
+      const res = await jsonPlaceHolder.get(`/users/${id}`)
+
+      dispatch({
+        type: 'FETCH_USER',
+        payload: res.data
+      })
+    }
